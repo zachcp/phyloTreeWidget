653d33917e1c2eba6d2ed82fb71313230b8c84cc
zach charlop-powers <zach.charlop.powers@gmail.com>
2018-05-03 10:39:22 -0400

    update coercion code and error checks for coercion of phylo and phylo4 objects

--- R/coerce_to_phy4d.R
+++ R/coerce_to_phy4d.R
@@ -38,7 +38,31 @@ setMethod("coerce_to_phy4d", signature(phy="phylo4", data="NULL"), function(phy,
 #' @export
 #' @importFrom phylobase phylo4d
 setMethod("coerce_to_phy4d", signature(phy="phylo4", data="data.frame"), function(phy, data) {
-  phylo4d(phy, data)
+
+  phy4 <-  try(phylo4d(phy, tip.data=data))
+  if (!is(phy4, "try-error")) return(phy4)
+
+  phy4 <-  try(phylo4d(phy, all.data=data))
+  if (!is(phy4, "try-error")) return(phy4)
+
+  phy4 <-  try(phylo4d(phy, node.data=data))
+  if (!is(phy4, "try-error")) return(phy4)
+
+  stop("Unable to merge your tree and data objects")
+
 })
 
+
+#' @export
+setMethod("coerce_to_phy4d", signature(phy="phylo4d", data="NULL"), function(phy, data) {
+  phy
+})
+
+
+#' @export
+#' @importFrom phylobase phylo4d
+setMethod("coerce_to_phy4d", signature(phy="phylo4d", data="data.frame"), function(phy, data) {
+  message("Your input phylo object is of class 'phylo4d' and as asuch it has data associated with it. You are attempting to add data to it. Please review your inputs. We are using the phylo4d object as-is")
+  phy
+})
 
--- tests/testthat/test_treetype_coercion.R
+++ tests/testthat/test_treetype_coercion.R
@@ -3,7 +3,7 @@ library(phylotree)
 context("Testing Coercion of Tree Types")
 
 
-test_that("handle data converison of phylo trees to phylo4d", {
+test_that("handle data conversion of phylo trees to phylo4d", {
 
   tree   <- ape::rtree(200)
   df     <- data.frame(column1=tree$tip.label,
@@ -28,6 +28,34 @@ test_that("handle data converison of phylo trees to phylo4d", {
                   c("label", "node", "ancestor", "edge.length", "node.type",
                     "column1","column2", "column3"))
 
-
 })
 
+
+test_that("handle phylo4 inputs", {
+
+  data(geospiza)
+
+  geospizadata <-  phylobase:::.phylo4ToDataFrame(geospiza)
+  expect_equal(length(names(geospizadata)), 10)
+
+  # remove the extra data
+  geophy4  <- as(geospiza, "phylo4")
+  phy1     <- coerce_to_phy4d(geophy4, data=NULL)
+  phy1data <- phylobase:::.phylo4ToDataFrame(phy1)
+
+  expect_is(phy1, "phylo4d")
+  expect_equal(length(names(phy1data)), 7)
+  expect_setequal(names(phy1data),
+                  c("label", "node", "ancestor", "edge.length", "node.type",
+                    "node", "col1"))
+
+  geophy4data <- tdata(geospiza)
+  phy2        <- coerce_to_phy4d(geophy4, data=geophy4data)
+  phy2data    <- phylobase:::.phylo4ToDataFrame(phy2)
+  expect_is(phy2, "phylo4d")
+  expect_equal(length(names(phy2data)), 10)
+  expect_setequal(names(phy2data),
+                  c("label", "node", "ancestor", "edge.length", "node.type",
+                    "wingL", "tarsusL",  "culmenL", "beakD", "gonysW"))
+
+})
