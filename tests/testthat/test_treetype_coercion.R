74194067bf5ff69869414304db4e2eaf31430ecb
zach charlop-powers <zach.charlop.powers@gmail.com>
2018-05-08 20:29:57 -0400

    update tests and suggest tibble

--- tests/testthat/test_treetype_coercion.R
+++ tests/testthat/test_treetype_coercion.R
@@ -18,7 +18,7 @@ test_that("handle data conversion of phylo trees to phylo4d", {
   expect_equal(length(names(phy1data)), 7)
   expect_setequal(names(phy1data),
                   c("label", "node", "ancestor", "edge.length", "node.type",
-                    "node", "col1"))
+                    "col1", "col2"))
 
   phy2     <- coerce_to_phy4d(tree, data=df)
   phy2data <- phylobase:::.phylo4ToDataFrame(phy2)
@@ -47,15 +47,16 @@ test_that("handle phylo4 inputs", {
   expect_equal(length(names(phy1data)), 7)
   expect_setequal(names(phy1data),
                   c("label", "node", "ancestor", "edge.length", "node.type",
-                    "node", "col1"))
+                    "col1", "col2"))
 
   geophy4data <- tdata(geospiza)
+  geophy4data <- tibble::rownames_to_column(geophy4data, var = "strain")
   phy2        <- coerce_to_phy4d(geophy4, data=geophy4data)
   phy2data    <- phylobase:::.phylo4ToDataFrame(phy2)
   expect_is(phy2, "phylo4d")
-  expect_equal(length(names(phy2data)), 10)
+  expect_equal(length(names(phy2data)), 11)
   expect_setequal(names(phy2data),
                   c("label", "node", "ancestor", "edge.length", "node.type",
-                    "wingL", "tarsusL",  "culmenL", "beakD", "gonysW"))
+                    "strain", "wingL", "tarsusL",  "culmenL", "beakD", "gonysW"))
 
 })
